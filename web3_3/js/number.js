a50d7e866bb24847b0becbaf91d6d90cdaedc4c8
emceein <amyrs4ai@gmail.com>
2023-11-26 23:51:55 +0330

    Number

--- web3_3/js/number.js
+++ web3_3/js/number.js
@@ -1,37 +1,4 @@
-// var x = 123e5;  
-// console.log(x);
 
-// var y = 123e-5;
-// console.log(y);
-
-// let numb1 = 42;
-// let str = numb1.toString(); // = "42"
-// console.log(str);
-
-// let strs = "3.14";
-// let numb2 = parseFloat(strs); // = 3.14
-// console.log(numb2);
-
-// let num1 = 3.1419;
-// let rounded = num1.toFixed(2); // = "3.14"
-// console.log(rounded);
-
-// let num2 = 123.456;
-// let prec = num2.toPrecision(4); // = "123.5"
-// console.log(prec);
-
-
-// let num = 123.456;
-// console.log(num.toPrecision(3)); 
-
-
-// console.log(parseInt("123.456")); 
-
-
-// console.log(Number.isNaN(NaN));
-
-
-// console.log(Number.isNaN(123)); 
 
 var m = 123e5;
 console.log(m);
